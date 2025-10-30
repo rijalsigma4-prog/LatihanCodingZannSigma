e48afd7c5e488a405783523f68774ad88dfd832b
ZannSigma11 <rijalsigma4@gmail.com>
2025-10-30 11:23:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementById('sapaBotton'):
-tombolSapa.addEvenListerner('click,function(){
+const tombolSapa = document.getElementById('sapaBotton');
+tombolSapa.addEvenListerner('click', function() {
   alert('Halo! Terima Kasih Sudah Berkunjung!'):
-                            });
+});
