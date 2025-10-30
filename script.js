3f2d40af50465db57be9eaa45f1f21ff9e06d3cf
ZannSigma11 <rijalsigma4@gmail.com>
2025-10-30 11:11:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const tombolSapa = document.getElementById('sapaBotton'):
+tombolSapa.addEvenListerner('click,function(){
+  alert('Halo! Terima Kasih Sudah Berkunjung!'):
+                            });
